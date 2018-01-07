01c124d7081e3a342c53c6ac38ff2c62e57f936e
shinhaha <akrja4@nate.com>
2018-01-07 15:57:11 +0900

    CotsMinimizeGrpahBuild
    CotsMinimizeGrpahBuild

---
+++ Inflearn_SungKim/CostMinimize/CotsMinimizeGrpahBuild.py
@@ -0,0 +1,25 @@
+import tensorflow as tf
+import matplotlib.pyplot as plt
+X=[1,2,3]
+Y=[1,2,3]
+
+W=tf.placeholder(tf.float32)
+#H(x)=Wx
+hypothesis=X*W
+#cost function
+cost=tf.reduce_mean(tf.square(hypothesis-Y))
+sess=tf.Session()
+#initialize
+sess.run(tf.global_variables_initializer())
+
+W_val=[]
+cost_val=[]
+for i in range(-30,50):
+    feed_W=i*0.1
+    curr_cost,curr_W=sess.run([cost,W],feed_dict={W:feed_W})
+    W_val.append(curr_W)
+    cost_val.append(curr_cost)
+
+#Show cost function 
+plt.plot(W_val,cost_val)
+plt.show()
